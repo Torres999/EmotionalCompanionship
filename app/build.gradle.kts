cff9c5676d419ac22c1636c96880fa81825392f4
Neo <neo@email.com>
2025-03-30 16:21:07 +0800

    统一页面布局实现方式

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,11 +41,7 @@ android {
     }
     buildFeatures {
         viewBinding = true
-        compose = true
         dataBinding = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
     }
     packaging {
         resources {
@@ -74,22 +70,6 @@ dependencies {
     // Gson
     implementation("com.google.code.gson:gson:2.10.1")
     
-    // Compose
-    val composeBom = platform("androidx.compose:compose-bom:2024.02.00")
-    implementation(composeBom)
-    androidTestImplementation(composeBom)
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation("androidx.compose.runtime:runtime")
-    implementation("androidx.compose.runtime:runtime-livedata")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation("androidx.navigation:navigation-compose:2.7.7")
-    implementation("androidx.compose.material:material-icons-extended:1.6.1")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-    
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-android-compiler:2.50")
     
@@ -102,5 +82,4 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
--- app/src/main/java/com/emotional/companionship/ui/navigation/Screen.kt
+++
@@ -1,7 +0,0 @@
-package com.emotional.companionship.ui.navigation
-
-sealed class Screen(val route: String) {
-    object Home : Screen("navigation_home")
-    object Memory : Screen("navigation_history")
-    object Profile : Screen("navigation_profile")
-} 
